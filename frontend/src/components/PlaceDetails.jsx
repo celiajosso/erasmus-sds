98e2b1138a536fa9f71644035169ce0b68416ea1
Lucas Tommi <tommi.lucas@etudiant.univ-rennes1.fr>
2025-05-05 19:49:55 +0200

    add all prices to the database and a great display on the frontend

--- frontend/src/components/PlaceDetails.jsx
+++ frontend/src/components/PlaceDetails.jsx
@@ -124,7 +124,7 @@ export default function PlaceDetailsPage() {
           <h2 className="text-xl text-[#282c34] font-bold mb-3 flex items-center gap-2">
             💳 <span>Prices</span>
           </h2>
-          <p className="text-gray-600 leading-relaxed">
+          <p className="text-gray-600 leading-relaxed whitespace-pre-line">
             {details.price || "No prices details provided."}
           </p>
         </div>
