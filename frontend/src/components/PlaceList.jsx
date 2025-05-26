7f8159d7f799fd31212b97217fc83e4bf9bd7138
celiajosso <celia.josso@gmail.com>
2025-05-26 14:49:17 +0200

    add heroicon icons

--- frontend/src/components/PlaceList.jsx
+++ frontend/src/components/PlaceList.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 import Header from './general/Header';
+import { HeartIcon as HeartIconSolid, PlusIcon } from '@heroicons/react/24/solid';
+import { HeartIcon as HeartIconOutline } from '@heroicons/react/24/outline';
 
 const PlaceList = () => {
   const [places, setPlaces] = useState([]);
@@ -320,15 +322,9 @@ const PlaceList = () => {
                   title={isFavorite(place.id) ? "Remove from favorites" : "Add to favorites"}
                 >
                   {isFavorite(place.id) ? (
-                    // Filled heart
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-10">
-                      <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
-                    </svg>
+                    <HeartIconSolid className="size-8"  />
                   ) : (
-                    // Outline heart
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-10">
-                      <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
-                    </svg>
+                    <HeartIconOutline className="size-8"  />
                   )}
                 </button>
 
@@ -338,7 +334,7 @@ const PlaceList = () => {
                  onClick={() => handleAddToPlaylist(place.id)}               
                   className="btn btn-accent"
                 >
-                  +
+                  <PlusIcon className="size-4" />
                 </button>
               </div>
             </div>
